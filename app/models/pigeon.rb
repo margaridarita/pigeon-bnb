a5902b9e47c5bb5ada9848347986246e9b3f356f
margarida rita <margaridabr@outlook.pt>
2023-02-28 14:41:42 +0000

    Removed user_id from controller pigeons

--- app/models/pigeon.rb
+++ app/models/pigeon.rb
@@ -2,7 +2,7 @@ class Pigeon < ApplicationRecord
   belongs_to :user
   has_many :bookings
 
-  validates :name, :description, :available, :price_per_day, presence: true
+  validates :name, :description, :price_per_day, presence: true
   validates :name, length: { maximum: 15 }
-  validates :description, length: { maximum: 50 }
+  validates :description, length: { maximum: 250 }
 end
