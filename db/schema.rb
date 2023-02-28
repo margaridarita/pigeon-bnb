f21dd27dad759a6c55ea756dcf3894ae3c66127e
Clatirissa Batista <clarissapbatista@gmail.com>
2023-02-28 11:51:24 +0000

    pages view

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_27_172803) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "pigeons", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.boolean "available"
+    t.float "price_per_day"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_pigeons_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -26,4 +37,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_27_172803) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "pigeons", "users"
 end
