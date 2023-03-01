b4d633ad1d1f564700edd6f677f49fb11bc6f0c6
Maria Salgado <maria.salgado2010@gmail.com>
2023-03-01 15:57:15 +0000

    carousel done

--- db/schema.rb
+++ db/schema.rb
@@ -40,7 +40,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_28_105452) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "username", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
