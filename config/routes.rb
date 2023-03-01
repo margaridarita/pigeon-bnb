aeab865f15f45bf7ed19b6fbf3c0d1f4704a3913
Sofia Magalhaes Clara <sofiamfclara@gmail.com>
2023-03-01 15:41:51 +0000

    edit bookings

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   resources :pigeons do
-    resources :bookings
+    resources :bookings, only: %i[new create]
   end
+  resources :bookings, only: %i[edit update show]
 end
