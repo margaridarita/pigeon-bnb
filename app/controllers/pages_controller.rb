5fefdf53b8112e0cc9c98c2a0f103cb663dfd447
Sofia Magalhaes Clara <sofiamfclara@gmail.com>
2023-03-02 09:41:07 +0000

    delete pigeons, delete bookings, dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,18 +1,26 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
+  before_action :set_user, only: %i[dashboard my_pigeons my_bookings]
 
-  def home
-  end
+  def home; end
 
   def dashboard
-    @user = current_user
+    @sum = 0
+    @user.pigeons.each do |pigeon|
+      b = Booking.where(pigeon_id: pigeon.id) if Booking.where(pigeon_id: pigeon.id).exists?
+      b.each do |bt|
+        @sum += bt.total_price
+      end
+    end
   end
 
-  def my_pigeons
-    @user = current_user
-  end
+  def my_pigeons; end
 
-  def my_bookings
+  def my_bookings; end
+
+  private
+
+  def set_user
     @user = current_user
   end
 end
